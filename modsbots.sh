40d895ebddefbb9d01e247f8ef3309c0bf0f10dc
modszf <botsmods75@gmail.com>
2024-06-21 21:30:17 +0630

    Create modsbots.sh

---
+++ modsbots.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -f web config.json
+wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip temp.zip
+sleep 2
+rm -f temp.zip
+sleep 2
+mv v2ray web
+sleep 2
+rm -rf config.json
+sleep 2
+wget https://raw.githubusercontent.com/PlayBillbes/render-me/main/config.json
+sleep 2
+wget https://github.com/eooce/choreo-2go/raw/main/files/server
+sleep 5
+chmod +x web server
+sleep 5
+nohup ./web run &>/dev/null &
+sleep 5
+nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiNmIwYzRiZDczMjQ4Y2IxNTYyMTdmN2QyNzZlOWE5ZjAiLCJ0IjoiNTkxNjI5YmUtNjI3MS00MGYwLWJjNmItNmIwMTZmYTIxMTE2IiwicyI6Ik56Y3lPVE5pTWpjdE5ERm1NeTAwWldReExXRXhOakV0WVdRMU5qTXpPVEF3WXpneSJ9 >/dev/null 2>&1 &
